01e28fd735a6bcb655b799d145ae37418c79dadf
div-tom1506 <divyanshuk1506@gmail.com>
2025-03-22 22:42:06 +0530

    developed userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,31 @@ const jwt = require('jsonwebtoken');
 const userController = require('../controller/userController');
 
 // fn for handling authentication token
+const secretKey = process.env.SECRET_KEY;
+const authenticateToken = (req, res, next) => {
+    const token = req.headers('Authorization');
+    if (!token) {
+        return res.status(401).json({message: 'Access denied'});
+    }
 
+    jwt.verify(token, secretKey, (err, user) => {
+        if(err){
+            return res.status(403).json({message: 'Invalid token'});
+        }
+        req.user = user;
+        next();
+    });
+}
 
 const router = express.Router();
 
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+router.get('/users', authenticateToken, userController.getAllUsers);
+router.get('/users/:id', authenticateToken, userController.getUserById);
+router.get('/users/username/:username', authenticateToken, userController.getUserById);
+router.get('/users/email/:email', authenticateToken, userController.getUserByEmail);
+router.put('users/update/:id', authenticateToken, userController.updateUser);
+router.delete('users/delete/:id', authenticateToken, userController.deleteUser);
+
+module.exports = router;
