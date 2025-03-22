01414e30bdb216c3cee20f1034d193bb1568dcb6
div-tom1506 <divyanshuk1506@gmail.com>
2025-03-23 01:41:14 +0530

    logging fix

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const morgan = require('morgan');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 // for http logging
 app.use(morgan('dev'));
 app.use('/api', userRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
